a0653b0928bc3c9a557800b07dc9913dfce63f06
Matthew <35896223+oseart55@users.noreply.github.com>
2025-05-31 19:36:26 -0700

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const select = document.getElementById("nav-select");
     const currentPath = window.location.pathname;
+    console.log(currentPath)
     for (let option of select.options) {
         if (option.value === currentPath) {
             option.selected = true;
