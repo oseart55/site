7a34016bb66de6eb412e4d8a0991f10a6eb95ccd
Matthew <35896223+oseart55@users.noreply.github.com>
2025-05-31 19:34:23 -0700

    changed nav bar

---
+++ js/navbar.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const select = document.getElementById("nav-select");
+    const currentPath = window.location.pathname;
+    for (let option of select.options) {
+        if (option.value === currentPath) {
+            option.selected = true;
+            break;
+        }
+    }
+});
